daa09d7d4e57e036934023b41e1dd1bb32fccc18
Peter Brunner <pbrunner@gmail.com>
2017-06-02 15:59:30 -0400

    Remove dependency on 'make'. Install tests and run them instead of just executing bfs when Testing bfs ==> Using the sandbox ==> ./tests.sh --bfs=/usr/local/Cellar/bfs/1.0.1/bin/bfs is called

--- pkg/brew/bfs.rb
+++ pkg/brew/bfs.rb
@@ -4,15 +4,20 @@ class Bfs < Formula
   homepage "https://github.com/tavianator/bfs"
   url "https://github.com/tavianator/bfs/archive/#{version}.tar.gz"
   sha256 "e4a1fd0bfc364960f87f67462fd48c9e1d09e43e9e8372ada774ee713d37a3e1"
-
-  depends_on "make" => :build
+  head "https://github.com/tavianator/bfs.git"
 
   def install
     system "make", "release"
+
     bin.install "bfs"
+
+    pkgshare.install "tests.sh"
+    pkgshare.install Dir["tests"]
   end
 
   test do
-    system bin/"bfs", "."
+    cp_r pkgshare/"tests", testpath
+    cp pkgshare/"tests.sh", testpath
+    system "./tests.sh", "--bfs=#{bin}/bfs"
   end
 end
