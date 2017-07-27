590c038630d453b3652b33e54b81fd06ceb0c6d2
Tavian Barnes <tavianator@tavianator.com>
2017-07-27 17:53:02 -0400

    Re-license under the BSD Zero Clause License

--- dstring.h
+++ dstring.h
@@ -1,13 +1,18 @@
-/*********************************************************************
- * bfs                                                               *
- * Copyright (C) 2016 Tavian Barnes <tavianator@tavianator.com>      *
- *                                                                   *
- * This program is free software. It comes without any warranty, to  *
- * the extent permitted by applicable law. You can redistribute it   *
- * and/or modify it under the terms of the Do What The Fuck You Want *
- * To Public License, Version 2, as published by Sam Hocevar. See    *
- * the COPYING file or http://www.wtfpl.net/ for more details.       *
- *********************************************************************/
+/****************************************************************************
+ * bfs                                                                      *
+ * Copyright (C) 2016-2017 Tavian Barnes <tavianator@tavianator.com>        *
+ *                                                                          *
+ * Permission to use, copy, modify, and/or distribute this software for any *
+ * purpose with or without fee is hereby granted.                           *
+ *                                                                          *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES *
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF         *
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  *
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES   *
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN    *
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  *
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           *
+ ****************************************************************************/
 
 #ifndef BFS_DSTRING_H
 #define BFS_DSTRING_H
