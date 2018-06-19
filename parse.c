tavianator/bfs

commit 7f8bacca4c2b1d35bb65ddf8cbf70fadf1adf66e

parse: Support -perm +7777, for compatibility with BSD and old GNU find

parse.c
@@ -1795,6 +1795,12 @@ static struct expr *parse_perm(struct parser_state *state, int field, int arg2) 
 		expr->mode_cmp = MODE_ANY;
 		++mode;
 		break;
+	case '+':
+		if (mode[1] >= '0' && mode[1] <= '9') {
+			expr->mode_cmp = MODE_ANY;
+			++mode;
+		}
+		break;
 	default:
 		expr->mode_cmp = MODE_EXACT;
 		break;

tests.sh
@@ -263,6 +263,8 @@ bsd_tests=(
     test_uid_name
     test_mnewer
     test_H_mnewer
+    test_perm_222_plus
+    test_perm_644_plus
     test_size_T
     test_quit
     test_quit_child
@@ -389,7 +391,6 @@ bfs_tests=(
     test_perm_symbolic_double_comma
     test_perm_symbolic_missing_action
     test_perm_leading_plus_symbolic
-    test_perm_octal_plus
     test_execdir_plus
     test_hidden
     test_nohidden
@@ -1000,6 +1001,10 @@ function test_perm_222_slash() {
     bfs_diff perms -perm /222
 }
 
+function test_perm_222_plus() {
+    bfs_diff perms -perm +222
+}
+
 function test_perm_644() {
     bfs_diff perms -perm 644
 }
@@ -1010,6 +1015,10 @@ function test_perm_644_minus() {
 
 function test_perm_644_slash() {
     bfs_diff perms -perm /644
+}
+
+function test_perm_644_plus() {
+    bfs_diff perms -perm +644
 }
 
 function test_perm_symbolic() {
@@ -1046,10 +1055,6 @@ function test_perm_leading_plus_symbolic_minus() {
 
 function test_perm_leading_plus_symbolic_slash() {
     bfs_diff perms -perm /+rwx
-}
-
-function test_perm_octal_plus() {
-    ! invoke_bfs perms -perm +777 2>/dev/null
 }
 
 function test_permcopy() {

tests/test_perm_222_plus.out
@@ -1,0 +1,5 @@
+perms
+perms/rw
+perms/rwx
+perms/w
+perms/wx

tests/test_perm_644_plus.out
@@ -1,0 +1,7 @@
+perms
+perms/r
+perms/rw
+perms/rwx
+perms/rx
+perms/w
+perms/wx

commit 07f7a1effc353ce66f268e26ba2625e20067dddb

parse: Don't leave expr->mode_cmp uninitialized
When adding support for -perm +7777, I introduced an unconditional break
that should have been conditional.  The bug was observable with

    $ ./tests.sh --verbose --bfs="valgrind $PWD/bfs" test_perm_leading_plus_symbolic

Fixes: 7f8bacca4c2b1d35bb65ddf8cbf70fadf1adf66e

parse.c
@@ -1826,8 +1826,8 @@ static struct expr *parse_perm(struct parser_state *state, int field, int arg2) 
 		if (mode[1] >= '0' && mode[1] <= '9') {
 			expr->mode_cmp = MODE_ANY;
 			++mode;
+			break;
 		}
-		break;
 	default:
 		expr->mode_cmp = MODE_EXACT;
 		break;