d61c4ed8551e5ef5642f93acec0b5c3f84ebede9
Tavian Barnes <tavianator@tavianator.com>
2019-04-28 12:27:56 -0400

    tests: Fix test_xtype_reorder

--- tests.sh
+++ tests.sh
@@ -1250,8 +1250,8 @@ function test_xtype_multi() {
 function test_xtype_reorder() {
     # Make sure -xtype is not reordered in front of anything -- if -xtype runs
     # before -links 100, it will report an ELOOP error
-    bfs_diff links -links 100 -xtype l
-    invoke_bfs links -links 100 -xtype l
+    bfs_diff loops -links 100 -xtype l
+    invoke_bfs loops -links 100 -xtype l
 }
 
 function test_iname() {
