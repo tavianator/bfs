f4ff4bce1ea6ae67609d8070117305dc9614b6d2
Tavian Barnes <tavianator@tavianator.com>
2019-06-28 17:18:14 -0400

    tests: Relax test_execdir_ulimit so that GNU find passes

--- tests.sh
+++ tests.sh
@@ -1420,11 +1420,11 @@ function test_execdir_slashes() {
 
 function test_execdir_ulimit() {
     rm -rf scratch/*
-    mkdir -p scratch/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t
-    mkdir -p scratch/a/b/c/d/e/f/g/h/i/j/0/1/2/3/4/5/6/7/8/9
+    mkdir -p scratch/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z
+    mkdir -p scratch/a/b/c/d/e/f/g/h/i/j/k/l/m/0/1/2/3/4/5/6/7/8/9/A/B/C
 
     closefrom 4
-    ulimit -n 10
+    ulimit -n 13
     bfs_diff scratch -execdir echo '{}' ';'
 }
 
