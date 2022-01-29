c898af13a671ee92a056467d324f16823594eb1c
Tavian Barnes <tavianator@tavianator.com>
2022-01-29 17:08:33 -0500

    tests: Add a test for posix-basic alternation

--- tests.sh
+++ tests.sh
@@ -622,6 +622,7 @@ gnu_tests=(
     test_regex
     test_regex_parens
     test_regex_error
+    test_regex_alternation
 
     test_regextype_posix_basic
     test_regextype_posix_extended
@@ -2033,6 +2034,10 @@ function test_E() {
 function test_regextype_posix_basic() {
     cd weirdnames
     bfs_diff -regextype posix-basic -regex '\./\((\)'
+}
+
+function test_regex_alternation() {
+    bfs_diff basic -regextype posix-basic -regex 'basic/\(a\|b\)'
 }
 
 function test_regextype_posix_extended() {
