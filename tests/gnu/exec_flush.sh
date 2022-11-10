f98a1c4a1cf61ff7d6483388ca1fac365fb0b31b
Tavian Barnes <tavianator@tavianator.com>
2022-11-10 11:02:19 -0500

    Revert "exec: Don't flush if the user says no to -ok"
    From the bfs_ctx_flush() implementation:
    
        // Before executing anything, flush all open streams.  This ensures that
        // - the user sees everything relevant before an -ok[dir] prompt
    
    But that only works if we call it before the prompt.
    
    This reverts commit 21344dfb72f9b65b366ca5f754f1bc35f52da3c7.

--- tests/gnu/exec_flush.sh
+++ tests/gnu/exec_flush.sh
@@ -1,4 +1,4 @@
-# IO streams should be flushed before executing programs
+# I/O streams should be flushed before executing programs
 invoke_bfs basic -print0 -exec echo found \; | tr '\0' ' ' >"$OUT"
 sort_output
 diff_output
