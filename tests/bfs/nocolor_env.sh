9cf8ea46ecc274351db419c2ab8fdf68c2d223d8
Tavian Barnes <tavianator@tavianator.com>
2024-01-01 23:46:11 -0500

    tests: Remove unneeded command -v unbuffer || skip

--- tests/bfs/nocolor_env.sh
+++ tests/bfs/nocolor_env.sh
@@ -1,5 +1,3 @@
-command -v unbuffer &>/dev/null || skip
-
 NO_COLOR=1 bfs_pty rainbow >"$OUT"
 sort_output
 diff_output
--- tests/bfs/nocolor_env_empty.sh
+++ tests/bfs/nocolor_env_empty.sh
@@ -1,5 +1,3 @@
-command -v unbuffer &>/dev/null || skip
-
 NO_COLOR= bfs_pty rainbow >"$OUT"
 sort_output
 diff_output
