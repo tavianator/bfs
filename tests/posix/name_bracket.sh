b7aa47b983f2d80a7a80d7c026d648767e0f18e4
Tavian Barnes <tavianator@tavianator.com>
2024-01-03 12:01:06 -0500

    tests/posix/name_bracket: Skip on NetBSD too

--- tests/posix/name_bracket.sh
+++ tests/posix/name_bracket.sh
@@ -1,5 +1,9 @@
-# fnmatch() is broken on macOS
-test "$UNAME" = "Darwin" && skip
+# fnmatch() is broken on some platforms
+case "$UNAME" in
+    Darwin|NetBSD)
+        skip
+        ;;
+esac
 
 # An unclosed [ should be matched literally
 bfs_diff weirdnames -name '['
