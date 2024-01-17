40e195c1bd60737964bb2b8ab25780a02003f8e1
Tavian Barnes <tavianator@tavianator.com>
2024-01-17 13:58:39 -0500

    tests/posix: Avoid catastrophic backtracking
    Using -path 'deep/*/*/.../*' to simulate -mindepth 18 falls off a
    performance cliff on systems that use backtracking for fnmatch().  This
    was observed on macOS 12.4.  Instead, just use -type f.

--- tests/posix/nogroup_ulimit.sh
+++ tests/posix/nogroup_ulimit.sh
@@ -1,7 +1,2 @@
 ulimit -n 16
-
-# -mindepth 18, but POSIX
-path="*/*/*/*/*/*"
-path="$path/$path/$path"
-bfs_diff deep -path "deep/$path" -nogroup
-
+bfs_diff deep -type f -nogroup
--- tests/posix/nouser_ulimit.sh
+++ tests/posix/nouser_ulimit.sh
@@ -1,6 +1,2 @@
 ulimit -n 16
-
-# -mindepth 18, but POSIX
-path="*/*/*/*/*/*"
-path="$path/$path/$path"
-bfs_diff deep -path "deep/$path" -nouser
+bfs_diff deep -type f -nouser
