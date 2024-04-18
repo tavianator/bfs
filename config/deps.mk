00a36e84e385bc02f04e335d8e5f786a2e962c9d
Tavian Barnes <tavianator@tavianator.com>
2024-04-18 08:03:43 -0400

    config: Remove explicit -MF from DEPFLAGS
    We use the default name anyway.

--- config/deps.mk
+++ config/deps.mk
@@ -12,7 +12,7 @@ ${GEN}/deps.mk::
 	${MSG} "[ GEN] ${TGT}"
 	printf '# %s\n' "${TGT}" >$@
 	if config/cc.sh -MD -MP -MF /dev/null config/empty.c; then \
-	    printf 'DEPFLAGS = -MD -MP -MF $${@:.o=.d}\n'; \
+	    printf 'DEPFLAGS = -MD -MP\n'; \
 	fi >>$@ 2>$@.log
 	${VCAT} $@
 	@printf -- '-include %s\n' ${OBJS:.o=.d} >>$@
