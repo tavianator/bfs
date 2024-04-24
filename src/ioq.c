tavianator/bfs

commit f64f76b55400b71e8576ed7e4a377eb5ef9576aa

ioq: Copy ring_ops from the previous thread
Otherwise threads 2-N won't use io_uring at all!  Oops.

Fixes: 8bc72d6c ("ioq: Probe for supported io_uring operations")

src/ioq.c
@@ -850,6 +850,7 @@ static int ioq_ring_init(struct ioq *ioq, struct ioq_thread *thread) {
 
 	if (prev) {
 		// Initial setup already complete
+		thread->ring_ops = prev->ring_ops;
 		return 0;
 	}
 
@@ -916,12 +917,14 @@ static void ioq_thread_join(struct ioq_thread *thread) {
 }
 
 struct ioq *ioq_create(size_t depth, size_t nthreads) {
-	struct ioq *ioq = ZALLOC_FLEX(struct ioq, threads, nthreads);
+	struct ioq *ioq = ALLOC_FLEX(struct ioq, threads, nthreads);
 	if (!ioq) {
 		goto fail;
 	}
 
 	ioq->depth = depth;
+	ioq->size = 0;
+	ioq->cancel = false;
 
 	ARENA_INIT(&ioq->ents, struct ioq_ent);
 

commit 19189e3f6f18b13e4f9c947c99062e658e98827d

ioq: Fix uninitialized values in ioq_create() cleanup path
I switched from ZALLOC_FLEX() to ALLOC_FLEX() in hopes that msan would
catch uninitialized values in ioq_thread_create(), but in doing so,
forgot to initialize all fields before the first goto fail.

Fixes: f64f76b ("ioq: Copy ring_ops from the previous thread")

src/ioq.c
@@ -918,17 +918,14 @@ static void ioq_thread_join(struct ioq_thread *thread) {
 }
 
 struct ioq *ioq_create(size_t depth, size_t nthreads) {
-	struct ioq *ioq = ALLOC_FLEX(struct ioq, threads, nthreads);
+	struct ioq *ioq = ZALLOC_FLEX(struct ioq, threads, nthreads);
 	if (!ioq) {
 		goto fail;
 	}
 
 	ioq->depth = depth;
-	ioq->size = 0;
-	ioq->cancel = false;
 
 	ARENA_INIT(&ioq->ents, struct ioq_ent);
-
 #if BFS_USE_LIBURING && BFS_USE_STATX
 	ARENA_INIT(&ioq->xbufs, struct statx);
 #endif