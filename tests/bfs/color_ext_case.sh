a39904098ff9e022bc755f36a9546cbf07a3ff49
Tavian Barnes <tavianator@tavianator.com>
2024-06-05 12:11:57 -0400

    tests: Improve color case insensitivity coverage

--- tests/bfs/color_ext_case.sh
+++ tests/bfs/color_ext_case.sh
@@ -1,6 +1,6 @@
-# *.gz=01;31:*.GZ=01;32         -- case sensitive
-# *.tAr=01;33:*.TaR=01;33       -- case-insensitive
-# *.TAR.gz=01;34:*.tar.GZ=01;35 -- case-sensitive
-# *.txt=35:*TXT=36              -- case-insensitive
-export LS_COLORS="*.gz=01;31:*.GZ=01;32:*.tAr=01;33:*.TaR=01;33:*.TAR.gz=01;34:*.tar.GZ=01;35:*.txt=35:*TXT=36"
+# *.gz=01;30:*.gz=01;31:*.GZ=01;30:*.GZ=01;32 -- case sensitive
+# *.tAr=01;33:*.TaR=01;33                     -- case-insensitive
+# *.TAR.gz=01;34:*.tar.GZ=01;35               -- case-sensitive
+# *.txt=35:*TXT=36                            -- case-insensitive
+export LS_COLORS="*.gz=01;30:*.gz=01;31:*.GZ=01;30:*.GZ=01;32:*.tAr=01;33:*.TaR=01;33:*.TAR.gz=01;34:*.tar.GZ=01;35:*.txt=35:*TXT=36"
 bfs_diff rainbow -color
--- tests/bfs/color_ext_override.sh
+++ tests/bfs/color_ext_override.sh
@@ -1,1 +1,1 @@
-LS_COLORS="*.tar.gz=01;31:*.TAR=01;32:*.gz=01;33:" bfs_diff rainbow -color
+LS_COLORS="*.tar.gz=01;31:*.TAR=01;32:*.gz=01;30:*.gz=01;33:" bfs_diff rainbow -color
