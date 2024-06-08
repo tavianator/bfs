tavianator/bfs

commit 23e048793401900c8d1768b4ffb0fc4ff623ef3e

ci: Surface compiler warnings/errors as GitHub actions messages

.github/diag.sh
@@ -1,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Copyright © Tavian Barnes <tavianator@tavianator.com>
+# SPDX-License-Identifier: 0BSD
+
+# Convert compiler diagnostics to GitHub Actions messages
+# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-a-warning-message
+
+set -eu
+
+filter() {
+    sed -E 's/^(([^:]*):([^:]*):([^:]*): (warning|error): (.*))$/::\5 file=\2,line=\3,col=\4,title=Compiler \5::\6\
+\1/'
+}
+
+"$@" 2> >(filter >&2) | filter

.github/workflows/ci.yml
@@ -39,7 +39,7 @@ jobs:
 
       - name: Run tests
         run: |
-          make -j$(nproc) distcheck
+          .github/diag.sh make -j$(nproc) distcheck
 
       - uses: actions/upload-artifact@v4
         with:
@@ -63,7 +63,7 @@ jobs:
       - name: Run tests
         run: |
           jobs=$(sysctl -n hw.ncpu)
-          make -j$jobs distcheck
+          .github/diag.sh make -j$jobs distcheck
 
   freebsd:
     name: FreeBSD
@@ -87,7 +87,7 @@ jobs:
                 pkgconf \
                 tcl-wrapper
             sudo mount -t fdescfs none /dev/fd
-            make -j$(nproc) distcheck
+            .github/diag.sh make -j$(nproc) distcheck
 
       - uses: actions/upload-artifact@v4
         with:
@@ -116,7 +116,7 @@ jobs:
                 oniguruma
             jobs=$(sysctl -n hw.ncpu)
             ./configure MAKE=gmake
-            gmake -j$jobs check TEST_FLAGS="--sudo --verbose=skipped"
+            .github/diag.sh gmake -j$jobs check TEST_FLAGS="--sudo --verbose=skipped"
 
       - uses: actions/upload-artifact@v4
         with:
@@ -146,7 +146,7 @@ jobs:
                 tcl-expect
             jobs=$(sysctl -n hw.ncpu)
             ./configure
-            make -j$jobs check TEST_FLAGS="--sudo --verbose=skipped"
+            .github/diag.sh make -j$jobs check TEST_FLAGS="--sudo --verbose=skipped"
 
       - uses: actions/upload-artifact@v4
         with:
@@ -182,7 +182,7 @@ jobs:
             chown -R action:action .
             jobs=$(sysctl -n hw.ncpu)
             sudo -u action ./configure
-            sudo -u action make -j$jobs check TEST_FLAGS="--sudo --verbose=skipped"
+            sudo -u action .github/diag.sh make -j$jobs check TEST_FLAGS="--sudo --verbose=skipped"
 
       - uses: actions/upload-artifact@v4
         with:
@@ -219,7 +219,7 @@ jobs:
             chown -R action:staff .
             jobs=$(getconf NPROCESSORS_ONLN)
             sudo -u action ./configure MAKE=gmake
-            sudo -u action gmake -j$jobs check TEST_FLAGS="--sudo --verbose=skipped"
+            sudo -u action .github/diag.sh gmake -j$jobs check TEST_FLAGS="--sudo --verbose=skipped"
 
       - uses: actions/upload-artifact@v4
         with:

commit 0873b0458fd1717b9b089790bc2c46c73bf2bd50

ci: Actually fail if the build fails
Fixes: 23e0487 ("ci: Surface compiler warnings/errors as GitHub actions messages")

.github/diag.sh
@@ -13,4 +13,4 @@ filter() {
 \1/'
 }
 
-"$@" 2> >(filter >&2) | filter
+"$@" > >(filter) 2> >(filter >&2)