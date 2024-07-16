tavianator/bfs

commit 29ddac2bf64bb305b285db86015abebe8a0bd8b3

config: Check for posix_spawn_file_actions_addfchdir{,_np}()

src/xspawn.c
@@ -212,7 +212,7 @@ int bfs_spawn_adddup2(struct bfs_spawn *ctx, int oldfd, int newfd) {
  * but macOS and NetBSD resolve the PATH *before* file_actions (because there
  * posix_spawn() is its own syscall).
  */
-#define BFS_POSIX_SPAWNP_AFTER_FCHDIR !(__APPLE__ || __NetBSD_Prereq__(10, 0, 0))
+#define BFS_POSIX_SPAWNP_AFTER_FCHDIR !(__APPLE__ || __NetBSD__)
 
 int bfs_spawn_addfchdir(struct bfs_spawn *ctx, int fd) {
 	struct bfs_spawn_action *action = bfs_spawn_action(BFS_SPAWN_FCHDIR);
@@ -220,29 +220,15 @@ int bfs_spawn_addfchdir(struct bfs_spawn *ctx, int fd) {
 		return -1;
 	}
 
-#ifndef BFS_HAS_POSIX_SPAWN_FCHDIR
-#  define BFS_HAS_POSIX_SPAWN_FCHDIR __NetBSD_Prereq__(10, 0, 0)
-#endif
-
-#ifndef BFS_HAS_POSIX_SPAWN_FCHDIR_NP
-#  if __GLIBC__
-#    define BFS_HAS_POSIX_SPAWN_FCHDIR_NP __GLIBC_PREREQ(2, 29)
-#  elif __ANDROID__
-#    define BFS_HAS_POSIX_SPAWN_FCHDIR_NP (__ANDROID_API__ >= 34)
-#  else
-#    define BFS_HAS_POSIX_SPAWN_FCHDIR_NP (__linux__ || __FreeBSD__ || __APPLE__)
-#  endif
-#endif
-
-#if BFS_HAS_POSIX_SPAWN_FCHDIR
-#  define BFS_POSIX_SPAWN_FCHDIR posix_spawn_file_actions_addfchdir
-#elif BFS_HAS_POSIX_SPAWN_FCHDIR_NP
-#  define BFS_POSIX_SPAWN_FCHDIR posix_spawn_file_actions_addfchdir_np
+#if BFS_HAS_POSIX_SPAWN_ADDFCHDIR
+#  define BFS_POSIX_SPAWN_ADDFCHDIR posix_spawn_file_actions_addfchdir
+#elif BFS_HAS_POSIX_SPAWN_ADDFCHDIR_NP
+#  define BFS_POSIX_SPAWN_ADDFCHDIR posix_spawn_file_actions_addfchdir_np
 #endif
 
 #if _POSIX_SPAWN > 0 && defined(BFS_POSIX_SPAWN_FCHDIR)
 	if (ctx->flags & BFS_SPAWN_USE_POSIX) {
-		errno = BFS_POSIX_SPAWN_FCHDIR(&ctx->actions, fd);
+		errno = BFS_POSIX_SPAWN_ADDFCHDIR(&ctx->actions, fd);
 		if (errno != 0) {
 			free(action);
 			return -1;

config/header.mk
@@ -10,7 +10,9 @@ include config/exports.mk
 # All header fragments we generate
 HEADERS := \
     ${GEN}/getprogname.h \
-    ${GEN}/getprogname-gnu.h
+    ${GEN}/getprogname-gnu.h \
+    ${GEN}/posix-spawn-addfchdir.h \
+    ${GEN}/posix-spawn-addfchdir-np.h
 
 ${GEN}/config.h: ${HEADERS}
 	${MSG} "[ GEN] ${TGT}"

config/posix-spawn-addfchdir-np.c
@@ -1,0 +1,11 @@
+// Copyright © Tavian Barnes <tavianator@tavianator.com>
+// SPDX-License-Identifier: 0BSD
+
+#include <spawn.h>
+
+int main(void) {
+	posix_spawn_file_actions_t actions;
+	posix_spawn_file_actions_init(&actions);
+	posix_spawn_file_actions_addfchdir_np(&actions, 3);
+	return 0;
+}

config/posix-spawn-addfchdir.c
@@ -1,0 +1,11 @@
+// Copyright © Tavian Barnes <tavianator@tavianator.com>
+// SPDX-License-Identifier: 0BSD
+
+#include <spawn.h>
+
+int main(void) {
+	posix_spawn_file_actions_t actions;
+	posix_spawn_file_actions_init(&actions);
+	posix_spawn_file_actions_addfchdir(&actions, 3);
+	return 0;
+}

config/prelude.mk
@@ -110,7 +110,7 @@ MSG = @msg() { \
       msg
 
 # Maximum width of a short message, to align the [X]
-MSG_WIDTH := 24
+MSG_WIDTH := 33
 
 # cat a file if V=1
 VCAT,y := @cat

commit d9f7bf31d0693b81bddb7e8c57118dd7f78a2952

xspawn: Test the right macro for posix_spawn_..._addfchdir() support
This should have been benign (just a performance regression), but
FreeBSD has a bug that causes the fork()-based fallback to lock up when
ASAN is enabled, due to fork() and dl_iterate_phdr().  This is not a
complete workaround for that issue, since the fork() fallback may be
used for other reasons.

Fixes: 29ddac2 ("config: Check for posix_spawn_file_actions_addfchdir{,_np}()")

src/xspawn.c
@@ -228,7 +228,7 @@ int bfs_spawn_addfchdir(struct bfs_spawn *ctx, int fd) {
 #  define BFS_POSIX_SPAWN_ADDFCHDIR posix_spawn_file_actions_addfchdir_np
 #endif
 
-#if _POSIX_SPAWN > 0 && defined(BFS_POSIX_SPAWN_FCHDIR)
+#if _POSIX_SPAWN > 0 && defined(BFS_POSIX_SPAWN_ADDFCHDIR)
 	if (ctx->flags & BFS_SPAWN_USE_POSIX) {
 		errno = BFS_POSIX_SPAWN_ADDFCHDIR(&ctx->actions, fd);
 		if (errno != 0) {