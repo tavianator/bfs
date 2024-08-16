tavianator/bfs

commit 33b85e1f8769e7f75721887638ae454d109a034f

Implement POSIX 2024's -mount
This reverts commit 4f80c17192f2b28c96a489969d4435151d68d0ce.

Link: https://www.austingroupbugs.net/view.php?id=1133

src/parse.c
@@ -1694,10 +1694,7 @@ static struct bfs_expr *parse_mount(struct bfs_parser *parser, int arg1, int arg
 		return NULL;
 	}
 
-	parse_expr_warning(parser, expr, "In the future, ${blu}%s${rs} will skip mount points entirely, unlike\n", expr->argv[0]);
-	bfs_warning(parser->ctx, "${blu}-xdev${rs}, due to http://austingroupbugs.net/view.php?id=1133.\n\n");
-
-	parser->ctx->flags |= BFTW_PRUNE_MOUNTS;
+	parser->ctx->flags |= BFTW_SKIP_MOUNTS;
 	parser->mount_arg = expr->argv;
 	return expr;
 }

tests/common/L_mount.out
@@ -1,5 +1,0 @@
-.
-./foo
-./foo/bar
-./foo/qux
-./mnt

tests/posix/L_mount.out
@@ -1,0 +1,2 @@
+.
+./foo

tests/posix/mount.out
@@ -1,4 +1,3 @@
 .
 ./foo
 ./foo/bar
-./mnt

commit 218e074be676afb77697717eebc0dcc713af2875

docs: Document the new -mount vs. -xdev behaviour
Fixes: 33b85e1 ("Implement POSIX 2024's -mount")

completions/bfs.fish
@@ -42,7 +42,7 @@ complete -c bfs -o ignore_readdir_race -d "Don't report an error if the file tre
 complete -c bfs -o noignore_readdir_race -d "Report an error if the file tree is modified during the search"
 complete -c bfs -o maxdepth -d "Ignore files deeper than specified number" -x
 complete -c bfs -o mindepth -d "Ignore files shallower than specified number" -x
-complete -c bfs -o mount -d "Don't descend into other mount points"
+complete -c bfs -o mount -d "Exclude mount points"
 complete -c bfs -o noerror -d "Ignore any errors that occur during traversal"
 complete -c bfs -o nohidden -d "Exclude hidden files and directories"
 complete -c bfs -o noleaf -d "Ignored; for compatibility with GNU find"

completions/bfs.zsh
@@ -41,7 +41,7 @@ args=(
     '*-noignore_readdir_race[do not report an error if bfs detects file tree is modified during search]'
     '*-maxdepth[ignore files deeper than N]:maximum search depth'
     '*-mindepth[ignore files shallower than N]:minimum search depth'
-    "*-mount[don't descend into other mount points]"
+    "*-mount[exclude mount points]"
     '*-noerror[ignore any errors that occur during traversal]'
     '*-nohidden[exclude hidden files]'
     '*-noleaf[ignored, for compatibility with GNU find]'

docs/bfs.1
@@ -316,9 +316,7 @@ Ignore files deeper/shallower than
 .RE
 .TP
 .B \-mount
-Don't descend into other mount points (same as
-.B \-xdev
-for now, but will skip mount points entirely in the future).
+Exclude mount points entirely from the results.
 .TP
 .B \-noerror
 Ignore any errors that occur during traversal.
@@ -384,6 +382,9 @@ Turn on or off warnings about the command line.
 .TP
 .B \-xdev
 Don't descend into other mount points.
+Unlike
+.BR \-mount ,
+the mount point itself is still included.
 .SH TESTS
 .TP
 .B \-acl

src/parse.c
@@ -2765,8 +2765,7 @@ static struct bfs_expr *parse_help(struct bfs_parser *parser, int arg1, int arg2
 	cfprintf(cout, "  ${blu}-mindepth${rs} ${bld}N${rs}\n");
 	cfprintf(cout, "      Ignore files deeper/shallower than ${bld}N${rs}\n");
 	cfprintf(cout, "  ${blu}-mount${rs}\n");
-	cfprintf(cout, "      Don't descend into other mount points (same as ${blu}-xdev${rs} for now, but will\n");
-	cfprintf(cout, "      skip mount points entirely in the future)\n");
+	cfprintf(cout, "      Exclude mount points entirely from the results\n");
 	cfprintf(cout, "  ${blu}-noerror${rs}\n");
 	cfprintf(cout, "      Ignore any errors that occur during traversal\n");
 	cfprintf(cout, "  ${blu}-nohidden${rs}\n");