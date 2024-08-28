tavianator/bfs

commit 3552b793e3b30f2bba0e77fbda45a86575f4dae6

build/flags: Infrastructure to detect compiler flag support
Use it to detect -MD -MP support.

build/flags-if.sh
@@ -1,0 +1,16 @@
+#!/bin/sh
+
+# Copyright © Tavian Barnes <tavianator@tavianator.com>
+# SPDX-License-Identifier: 0BSD
+
+# Add flags to a makefile if a build succeeds
+
+set -eu
+
+FLAGS=$(sed -n '\|^///|{s|^/// ||; s|[^=]*= ||; p}' "$1")
+
+if build/cc.sh "$@" $FLAGS; then
+    sed -n 's|^/// \(.*=.*\)|\1|p' "$1"
+else
+    exit 1
+fi

build/deps.mk
@@ -11,8 +11,5 @@ include build/exports.mk
 gen/deps.mk::
 	${MSG} "[ GEN] $@"
 	@printf '# %s\n' "$@" >$@
-	@if build/cc.sh -MD -MP build/empty.c -o gen/.deps.out; then \
-	    printf '_CPPFLAGS += -MD -MP\n'; \
-	fi >>$@ 2>$@.log
-	${VCAT} $@
 	@printf -- '-include %s\n' ${OBJS:.o=.d} >>$@
+	${VCAT} $@

build/flags.mk
@@ -103,7 +103,11 @@ _LDLIBS := ${LDLIBS} ${EXTRA_LDLIBS} ${_LDLIBS}
 
 include build/exports.mk
 
-gen/flags.mk::
+# Conditionally-supported flags
+AUTO_FLAGS := \
+    gen/flags/deps.mk
+
+gen/flags.mk: ${AUTO_FLAGS}
 	${MSG} "[ GEN] $@"
 	@printf '# %s\n' "$@" >$@
 	@printf '_CPPFLAGS := %s\n' "$$XCPPFLAGS" >>$@
@@ -112,4 +116,18 @@ gen/flags.mk::
 	@printf '_LDLIBS := %s\n' "$$XLDLIBS" >>$@
 	@printf 'NOLIBS := %s\n' "$$XNOLIBS" >>$@
 	@test "${OS}-${SAN}" != FreeBSD-y || printf 'POSTLINK = elfctl -e +noaslr $$@\n' >>$@
+	@cat ${.ALLSRC} >>$@
 	${VCAT} $@
+.PHONY: gen/flags.mk
+
+# The short name of the config test
+SLUG = ${@:gen/%.mk=%}
+# The source file to build
+CSRC = build/${SLUG}.c
+# The hidden output file name
+OUT = ${SLUG:flags/%=gen/flags/.%.out}
+
+${AUTO_FLAGS}::
+	@${MKDIR} ${@D}
+	@build/flags-if.sh ${CSRC} -o ${OUT} >$@ 2>$@.log; \
+	    build/msg-if.sh "[ CC ] ${SLUG}.c" test $$? -eq 0

build/flags/deps.c
@@ -1,0 +1,8 @@
+// Copyright © Tavian Barnes <tavianator@tavianator.com>
+// SPDX-License-Identifier: 0BSD
+
+/// _CPPFLAGS += -MD -MP
+
+int main(void) {
+	return 0;
+}

commit 34e60816adb0ea8ddb155a454676a99ab225dc8a

build/flags-if: Fix sed expression on BSD
`p` needs a terminating semicolon; otherwise we get

    sed: 1: "\|^///|{s|^/// ||; s|[^ ...": extra characters at the end of p command

Fixes: 3552b79 ("build/flags: Infrastructure to detect compiler flag support")

build/flags-if.sh
@@ -8,7 +8,7 @@
 set -eu
 
 OLD_FLAGS="$XCC $XCPPFLAGS $XCFLAGS $XLDFLAGS $XLDLIBS"
-NEW_FLAGS=$(sed -n '\|^///|{s|^/// ||; s|[^=]*= ||; p}' "$1")
+NEW_FLAGS=$(sed -n '\|^///|{s|^/// ||; s|[^=]*= ||; p;}' "$1")
 build/cc.sh "$@" $NEW_FLAGS || exit 1
 
 # De-duplicate against the existing flags