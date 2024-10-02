tavianator/bfs

commit 08030aea919039165c02805e8c637a9ec1ad0d70

color: Delay the case_sensitive decision

src/color.c
@@ -214,57 +214,26 @@ static void ext_tolower(char *ext, size_t len) {
 	}
 }
 
-/**
- * The "smart case" algorithm.
- *
- * @param ext
- *         The current extension being added.
- * @param prev
- *         The previous case-sensitive match, if any, for the same extension.
- * @param iprev
- *         The previous case-insensitive match, if any, for the same extension.
- * @return
- *         Whether this extension should become case-sensitive.
- */
-static bool ext_case_sensitive(struct ext_color *ext, struct ext_color *prev, struct ext_color *iprev) {
-	// This is the first case-insensitive occurrence of this extension, e.g.
-	//
-	//     *.gz=01;31:*.tar.gz=01;33
-	if (!iprev) {
-		bfs_assert(!prev);
-		return false;
+/** Insert an extension into a trie. */
+static int insert_ext(struct trie *trie, struct ext_color *ext) {
+	// A later *.x should override any earlier *.x, *.y.x, etc.
+	struct trie_leaf *leaf;
+	while ((leaf = trie_find_postfix(trie, ext->ext))) {
+		trie_remove(trie, leaf);
 	}
 
-	// If the last version of this extension is already case-sensitive,
-	// this one should be too, e.g.
-	//
-	//     *.tar.gz=01;31:*.TAR.GZ=01;32:*.TAR.GZ=01;33
-	if (iprev->case_sensitive) {
-		return true;
+	size_t len = ext->len + 1;
+	leaf = trie_insert_mem(trie, ext->ext, len);
+	if (!leaf) {
+		return -1;
 	}
 
-	// The case matches the last occurrence exactly, e.g.
-	//
-	//     *.tar.gz=01;31:*.tar.gz=01;33
-	if (iprev == prev) {
-		return false;
-	}
-
-	// Different case, but same value, e.g.
-	//
-	//     *.tar.gz=01;31:*.TAR.GZ=01;31
-	if (esc_eq(iprev->esc, ext->esc->seq, ext->esc->len)) {
-		return false;
-	}
-
-	// Different case, different value, e.g.
-	//
-	//     *.tar.gz=01;31:*.TAR.GZ=01;33
-	return true;
+	leaf->value = ext;
+	return 0;
 }
 
 /** Set the color for an extension. */
-static int set_ext(struct colors *colors, char *key, char *value) {
+static int set_ext(struct colors *colors, dchar *key, dchar *value) {
 	size_t len = dstrlen(key);
 	struct ext_color *ext = varena_alloc(&colors->ext_arena, len + 1);
 	if (!ext) {
@@ -279,45 +248,19 @@ static int set_ext(struct colors *colors, char *key, char *value) {
 		goto fail;
 	}
 
-	key = memcpy(ext->ext, key, len + 1);
+	memcpy(ext->ext, key, len + 1);
 
 	// Reverse the extension (`*.y.x` -> `x.y.*`) so we can use trie_find_prefix()
-	ext_reverse(key, len);
-
-	// Find any pre-existing exact match
-	struct ext_color *prev = NULL;
-	struct trie_leaf *leaf = trie_find_str(&colors->ext_trie, key);
-	if (leaf) {
-		prev = leaf->value;
-		trie_remove(&colors->ext_trie, leaf);
-	}
-
-	// A later *.x should override any earlier *.x, *.y.x, etc.
-	while ((leaf = trie_find_postfix(&colors->ext_trie, key))) {
-		trie_remove(&colors->ext_trie, leaf);
-	}
+	ext_reverse(ext->ext, len);
 
 	// Insert the extension into the case-sensitive trie
-	leaf = trie_insert_str(&colors->ext_trie, key);
-	if (!leaf) {
-		goto fail;
-	}
-	leaf->value = ext;
-
-	// "Smart case": if the same extension is given with two different
-	// capitalizations (e.g. `*.y.x=31:*.Y.Z=32:`), make it case-sensitive
-	ext_tolower(key, len);
-	leaf = trie_insert_str(&colors->iext_trie, key);
-	if (!leaf) {
+	if (insert_ext(&colors->ext_trie, ext) != 0) {
 		goto fail;
 	}
 
-	struct ext_color *iprev = leaf->value;
-	if (ext_case_sensitive(ext, prev, iprev)) {
-		iprev->case_sensitive = true;
-		ext->case_sensitive = true;
+	if (colors->ext_len < len) {
+		colors->ext_len = len;
 	}
-	leaf->value = ext;
 
 	return 0;
 
@@ -329,32 +272,83 @@ fail:
 	return -1;
 }
 
-/** Rebuild the case-insensitive trie after all extensions have been parsed. */
+/**
+ * The "smart case" algorithm.
+ *
+ * @param ext
+ *         The current extension being added.
+ * @param iext
+ *         The previous case-insensitive match, if any, for the same extension.
+ * @return
+ *         Whether this extension should become case-sensitive.
+ */
+static bool ext_case_sensitive(struct ext_color *ext, struct ext_color *iext) {
+	// This is the first case-insensitive occurrence of this extension, e.g.
+	//
+	//     *.gz=01;31:*.tar.gz=01;33
+	if (!iext) {
+		return false;
+	}
+
+	// If the last version of this extension is already case-sensitive,
+	// this one should be too, e.g.
+	//
+	//     *.tar.gz=01;31:*.TAR.GZ=01;32:*.TAR.GZ=01;33
+	if (iext->case_sensitive) {
+		return true;
+	}
+
+	// Different case, but same value, e.g.
+	//
+	//     *.tar.gz=01;31:*.TAR.GZ=01;31
+	if (esc_eq(iext->esc, ext->esc->seq, ext->esc->len)) {
+		return false;
+	}
+
+	// Different case, different value, e.g.
+	//
+	//     *.tar.gz=01;31:*.TAR.GZ=01;33
+	return true;
+}
+
+/** Build the case-insensitive trie, after all extensions have been parsed. */
 static int build_iext_trie(struct colors *colors) {
+	// Find which extensions should be case-sensitive
+	for_trie (leaf, &colors->ext_trie) {
+		struct ext_color *ext = leaf->value;
+
+		// "Smart case": if the same extension is given with two different
+		// capitalizations (e.g. `*.y.x=31:*.Y.Z=32:`), make it case-sensitive
+		ext_tolower(ext->ext, ext->len);
+
+		size_t len = ext->len + 1;
+		struct trie_leaf *ileaf = trie_insert_mem(&colors->iext_trie, ext->ext, len);
+		if (!ileaf) {
+			return -1;
+		}
+
+		struct ext_color *iext = ileaf->value;
+		if (ext_case_sensitive(ext, iext)) {
+			ext->case_sensitive = true;
+			iext->case_sensitive = true;
+		}
+
+		ileaf->value = ext;
+	}
+
+	// Rebuild the trie with only the case-insensitive ones
 	trie_clear(&colors->iext_trie);
 
 	for_trie (leaf, &colors->ext_trie) {
-		size_t len = leaf->length - 1;
-		if (colors->ext_len < len) {
-			colors->ext_len = len;
-		}
-
 		struct ext_color *ext = leaf->value;
 		if (ext->case_sensitive) {
 			continue;
 		}
 
-		// set_ext() already reversed and lowercased the extension
-		struct trie_leaf *ileaf;
-		while ((ileaf = trie_find_postfix(&colors->iext_trie, ext->ext))) {
-			trie_remove(&colors->iext_trie, ileaf);
-		}
-
-		ileaf = trie_insert_str(&colors->iext_trie, ext->ext);
-		if (!ileaf) {
+		// We already lowercased the extension above
+		if (insert_ext(&colors->iext_trie, ext) != 0) {
 			return -1;
 		}
-		ileaf->value = ext;
 	}
 
 	return 0;

tests/bfs/color_ext_case_flipflop.out
@@ -1,0 +1,27 @@
+[01;34m$'rainbow/\e[1m'[0m
+[01;34m$'rainbow/\e[1m/'[0m$'\e[0m'
+[01;34mrainbow[0m
+[01;34mrainbow/[0m[01;32mexec.sh[0m
+[01;34mrainbow/[0m[01;33mlower.tar.gz[0m
+[01;34mrainbow/[0m[01;33mlu.tar.GZ[0m
+[01;34mrainbow/[0m[01;33mul.TAR.gz[0m
+[01;34mrainbow/[0m[01;33mupper.TAR.GZ[0m
+[01;34mrainbow/[0m[01;35msocket[0m
+[01;34mrainbow/[0m[01;36mbroken[0m
+[01;34mrainbow/[0m[01;36mchardev_link[0m
+[01;34mrainbow/[0m[01;36mlink.txt[0m
+[01;34mrainbow/[0m[30;42msticky_ow[0m
+[01;34mrainbow/[0m[30;43msgid[0m
+[01;34mrainbow/[0m[33mpipe[0m
+[01;34mrainbow/[0m[34;42mow[0m
+[01;34mrainbow/[0m[37;41msugid[0m
+[01;34mrainbow/[0m[37;41msuid[0m
+[01;34mrainbow/[0m[37;44msticky[0m
+[01;34mrainbow/[0mfile.dat
+[01;34mrainbow/[0mfile.txt
+[01;34mrainbow/[0mlower.gz
+[01;34mrainbow/[0mlower.tar
+[01;34mrainbow/[0mmh1
+[01;34mrainbow/[0mmh2
+[01;34mrainbow/[0mupper.GZ
+[01;34mrainbow/[0mupper.TAR

tests/bfs/color_ext_case_flipflop.sh
@@ -1,0 +1,1 @@
+LS_COLORS="*.tar.gz=01;31:*.TAR.GZ=01;32:*.TAR.GZ=01;33:*.tar.gz=01;33:" bfs_diff rainbow -color

commit f5eaadb96fb94b2d3666e53a99495840a3099aec

color: Fix an assertion failure with embedded NUL bytes
Leading NUL bytes (e.g. `*\0.gz=...`) could cause us to insert a
non-prefix-free set of strings into the case-insensitive extension trie,
which would lead to crashes like

   bfs: trie_split@src/trie.c:538: Assertion failed: `key_nibble != rep_nibble`

and OOB accesses to trie leaf keys.  Fix it by ignoring those
extensions, since filenames cannot contain NUL bytes.

Fixes: 08030aea ("color: Delay the case_sensitive decision")

src/color.c
@@ -237,6 +237,16 @@ static int insert_ext(struct trie *trie, struct ext_color *ext) {
 /** Set the color for an extension. */
 static int set_ext(struct colors *colors, dchar *key, dchar *value) {
 	size_t len = dstrlen(key);
+
+	// Embedded NUL bytes in extensions can lead to a non-prefix-free
+	// set of strings, e.g. {".gz", "\0.gz"} would be transformed to
+	// {"zg.\0", "zg.\0\0"} (showing the implicit terminating NUL).
+	// Our trie implementation only supports prefix-free key sets, but
+	// luckily '\0' cannot appear in filenames so we can ignore them.
+	if (memchr(key, '\0', len)) {
+		return 0;
+	}
+
 	struct ext_color *ext = varena_alloc(&colors->ext_arena, len + 1);
 	if (!ext) {
 		return -1;

tests/bfs/color_ext_case_nul.out
@@ -1,0 +1,27 @@
+[01;34m$'rainbow/\e[1m'[0m
+[01;34m$'rainbow/\e[1m/'[0m$'\e[0m'
+[01;34mrainbow[0m
+[01;34mrainbow/[0m[01;31mlower.gz[0m
+[01;34mrainbow/[0m[01;31mlower.tar.gz[0m
+[01;34mrainbow/[0m[01;31mlu.tar.GZ[0m
+[01;34mrainbow/[0m[01;31mul.TAR.gz[0m
+[01;34mrainbow/[0m[01;31mupper.GZ[0m
+[01;34mrainbow/[0m[01;31mupper.TAR.GZ[0m
+[01;34mrainbow/[0m[01;32mexec.sh[0m
+[01;34mrainbow/[0m[01;35msocket[0m
+[01;34mrainbow/[0m[01;36mbroken[0m
+[01;34mrainbow/[0m[01;36mchardev_link[0m
+[01;34mrainbow/[0m[01;36mlink.txt[0m
+[01;34mrainbow/[0m[30;42msticky_ow[0m
+[01;34mrainbow/[0m[30;43msgid[0m
+[01;34mrainbow/[0m[33mpipe[0m
+[01;34mrainbow/[0m[34;42mow[0m
+[01;34mrainbow/[0m[37;41msugid[0m
+[01;34mrainbow/[0m[37;41msuid[0m
+[01;34mrainbow/[0m[37;44msticky[0m
+[01;34mrainbow/[0mfile.dat
+[01;34mrainbow/[0mfile.txt
+[01;34mrainbow/[0mlower.tar
+[01;34mrainbow/[0mmh1
+[01;34mrainbow/[0mmh2
+[01;34mrainbow/[0mupper.TAR

tests/bfs/color_ext_case_nul.sh
@@ -1,0 +1,5 @@
+# Regression test: embedded NUL bytes in an extension caused an assertion
+# failure in the trie implementation
+
+export LS_COLORS='*.gz=01;31:*\0.GZ=01;32:'
+bfs_diff rainbow -color