tavianator/bfs

commit b4c3201ccceb9c73dd7751d7f9937b4afe78966f

color: Only highlight the trailing slash on ENOTDIR

src/color.c
@@ -982,8 +982,10 @@ static ssize_t first_broken_offset(const char *path, const struct BFTW *ftwbuf, 
 		while (ret && at_path[len - 1] == '/') {
 			--len, --ret;
 		}
-		while (ret && at_path[len - 1] != '/') {
-			--len, --ret;
+		if (errno != ENOTDIR) {
+			while (ret && at_path[len - 1] != '/') {
+				--len, --ret;
+			}
 		}
 
 		dstresize(&at_path, len);

tests/bfs/color_ls.out
@@ -2,11 +2,11 @@
 [01;31mscratch/foo/bar[0m
 [01;34m/[0m[01;31m__bfs__/nowhere[0m
 [01;34m/[0m[01;31m__bfs__/nowhere[0m
-[01;34mfoo/bar/[0m[01;31mbaz/qux[0m
-[01;34mfoo/bar/[0m[01;31mbaz/qux[0m
 [01;34mfoo/bar/[0m[01;31mnowhere[0m
 [01;34mfoo/bar/[0m[01;31mnowhere[0m
 [01;34mfoo/bar/[0m[01;31mnowhere/nothing[0m
 [01;34mfoo/bar/[0m[01;31mnowhere/nothing[0m
 [01;34mfoo/bar/[0mbaz
 [01;34mfoo/bar/[0mbaz
+[01;34mfoo/bar/baz[0m[01;31m//qux[0m
+[01;34mfoo/bar/baz[0m[01;31m//qux[0m

tests/bfs/color_ls.sh
@@ -3,13 +3,13 @@ clean_scratch
 ln -s foo/bar/baz scratch/link
 ln -s foo/bar/nowhere scratch/broken
 ln -s foo/bar/nowhere/nothing scratch/nested
-ln -s foo/bar/baz/qux scratch/notdir
+ln -s foo/bar/baz//qux scratch/notdir
 ln -s scratch/foo/bar scratch/relative
 mkdir scratch/__bfs__
 ln -s /__bfs__/nowhere scratch/absolute
 
-LS_COLORS="or=01;31:" invoke_bfs scratch/{,link,broken,nested,notdir,relative,absolute} -color -type l -ls \
+export LS_COLORS="or=01;31:"
+invoke_bfs scratch/{,link,broken,nested,notdir,relative,absolute} -color -type l -ls \
     | sed 's/.* -> //' \
     | sort >"$OUT"
-
 diff_output

commit b89f22cbf250958a802915eb7b6bf0e5f38376ca

color: Fix an infinite loop colorizing some invalid paths
Previously, given

    $ touch file
    $ ln -s file/file notdir
    $ bfs notdir/file

bfs would loop forever when printing the error message, since it
expected stripping the trailing slash from "notdir/" to fix the ENOTDIR
error, but the broken symlink still gave the same error.

Fixes: b4c3201 ("color: Only highlight the trailing slash on ENOTDIR")

src/color.c
@@ -998,22 +998,25 @@ static ssize_t first_broken_offset(const char *path, const struct BFTW *ftwbuf, 
 		goto out;
 	}
 
+	size_t len = dstrlen(at_path);
 	while (ret > 0) {
+		dstresize(&at_path, len);
 		if (xfaccessat(at_fd, at_path, F_OK) == 0) {
 			break;
 		}
 
-		size_t len = dstrlen(at_path);
-		while (ret && at_path[len - 1] == '/') {
-			--len, --ret;
-		}
-		if (errno != ENOTDIR) {
-			while (ret && at_path[len - 1] != '/') {
+		// Try without trailing slashes, to distinguish "notdir/" from "notdir"
+		if (at_path[len - 1] == '/') {
+			do {
 				--len, --ret;
-			}
+			} while (ret > 0 && at_path[len - 1] == '/');
+			continue;
 		}
 
-		dstresize(&at_path, len);
+		// Remove the last component and try again
+		do {
+			--len, --ret;
+		} while (ret > 0 && at_path[len - 1] != '/');
 	}
 
 out_path:

tests/bfs/color_notdir_slash_error.sh
@@ -1,0 +1,2 @@
+# Regression test: infinite loop printing the error message for .../notdir/nowhere
+! bfs_diff -color links/notdir/nowhere