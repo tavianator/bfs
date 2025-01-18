tavianator/bfs

commit 423cfa40efe859448b69fbbbce62a5be750bc9fd

xspawn: Mask signals before fork()
This prevents signal handlers from running in the child before execve().

src/xspawn.c
@@ -5,9 +5,11 @@
 #include "xspawn.h"
 #include "alloc.h"
 #include "bfstd.h"
+#include "diag.h"
 #include "list.h"
 #include <errno.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/resource.h>
@@ -590,30 +592,49 @@ static pid_t bfs_fork_spawn(struct bfs_resolver *res, const struct bfs_spawn *ct
 		return -1;
 	}
 
+	// Block signals before fork() so handlers don't run in the child
+	sigset_t new_mask;
+	if (sigfillset(&new_mask) != 0) {
+		goto fail;
+	}
+	sigset_t old_mask;
+	errno = pthread_sigmask(SIG_BLOCK, &new_mask, &old_mask);
+	if (errno != 0) {
+		goto fail;
+	}
+
 	pid_t pid = fork();
-	if (pid < 0) {
-		close_quietly(pipefd[1]);
-		close_quietly(pipefd[0]);
-		return -1;
-	} else if (pid == 0) {
+	if (pid == 0) {
 		// Child
 		bfs_spawn_exec(res, ctx, argv, envp, pipefd);
 	}
 
-	// Parent
+	// Restore the original signal mask
+	int ret = pthread_sigmask(SIG_SETMASK, &old_mask, NULL);
+	bfs_verify(ret == 0, "pthread_sigmask(): %s", xstrerror(ret));
+
+	if (pid < 0) {
+		// fork() failed
+		goto fail;
+	}
+
 	xclose(pipefd[1]);
 
 	int error;
 	ssize_t nbytes = xread(pipefd[0], &error, sizeof(error));
 	xclose(pipefd[0]);
 	if (nbytes == sizeof(error)) {
-		int wstatus;
-		xwaitpid(pid, &wstatus, 0);
+		xwaitpid(pid, NULL, 0);
 		errno = error;
 		return -1;
 	}
 
 	return pid;
+
+fail:
+	close_quietly(pipefd[1]);
+	close_quietly(pipefd[0]);
+	return -1;
 }
 
 /** Call the right bfs_spawn() implementation. */

commit af207e702148e5c9ae08047d7a2dce6394653b62

xspawn: Unblock signals between fork() and exec()
Otherwise the child process runs with all signals blocked.

Fixes: 423cfa4 ("xspawn: Mask signals before fork()")

src/xspawn.c
@@ -8,6 +8,7 @@
 #include "bfstd.h"
 #include "diag.h"
 #include "list.h"
+#include "sighook.h"
 
 #include <errno.h>
 #include <fcntl.h>
@@ -535,7 +536,7 @@ static bool bfs_use_posix_spawn(const struct bfs_resolver *res, const struct bfs
 
 /** Actually exec() the new process. */
 _noreturn
-static void bfs_spawn_exec(struct bfs_resolver *res, const struct bfs_spawn *ctx, char **argv, char **envp, int pipefd[2]) {
+static void bfs_spawn_exec(struct bfs_resolver *res, const struct bfs_spawn *ctx, char **argv, char **envp, const sigset_t *mask, int pipefd[2]) {
 	xclose(pipefd[0]);
 
 	for_slist (const struct bfs_spawn_action, action, ctx) {
@@ -596,6 +597,18 @@ static void bfs_spawn_exec(struct bfs_resolver *res, const struct bfs_spawn *ctx
 		goto fail;
 	}
 
+	// Reset signal handlers to their original values before we unblock
+	// signals, so that handlers don't run in both the parent and the child
+	if (sigreset() != 0) {
+		goto fail;
+	}
+
+	// Restore the original signal mask for the child process
+	errno = pthread_sigmask(SIG_SETMASK, mask, NULL);
+	if (errno != 0) {
+		goto fail;
+	}
+
 	execve(res->exe, argv, envp);
 
 fail:;
@@ -635,7 +648,7 @@ static pid_t bfs_fork_spawn(struct bfs_resolver *res, const struct bfs_spawn *ct
 #endif
 	if (pid == 0) {
 		// Child
-		bfs_spawn_exec(res, ctx, argv, envp, pipefd);
+		bfs_spawn_exec(res, ctx, argv, envp, &old_mask, pipefd);
 	}
 
 	// Restore the original signal mask