465ac7ae544464f17d7a2dfcb993326ab2f00536
Tavian Barnes <tavianator@tavianator.com>
2026-06-29 13:45:02 -0400

    tests/posix/size_size: Just use printf %<SIZE>s to make files

--- tests/posix/size_size.sh
+++ tests/posix/size_size.sh
@@ -2,11 +2,20 @@
 
 cd "$TEST"
 
-printf '.' >./1
+printf '' >./0
+printf . >./1
 
-dd if=./1 of=./511 bs=1 seek=510
-dd if=./1 of=./512 bs=1 seek=511
-dd if=./1 of=./513 bs=1 seek=512
+printf '%255s' . >./255
+printf '%256s' . >./256
+printf '%257s' . >./257
+
+printf '%511s' . >./511
+printf '%512s' . >./512
+printf '%513s' . >./513
+
+printf '%1023s' . >./1023
+printf '%1024s' . >./1024
+printf '%1025s' . >./1025
 
 bfs_diff . -type f -size 1 -size 511c -exec printf '511: %s\n' {} \; \
     -o -type f -size -2 -size 512c -exec printf '512: %s\n' {} \; \
