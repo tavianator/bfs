4cbff23108d04b1f070166865e62a3d6e1b54663
Tavian Barnes <tavianator@tavianator.com>
2026-06-29 13:28:44 -0400

    tests/posix/size_size: Don't test directory sizes
    Fixes: a6aecebe ("opt: Take the -size unit into account")

--- tests/posix/size_size.sh
+++ tests/posix/size_size.sh
@@ -6,6 +6,6 @@ dd if=/dev/null of=511 bs=1 seek=511 count=0
 dd if=/dev/null of=512 bs=1 seek=512 count=0
 dd if=/dev/null of=513 bs=1 seek=513 count=0
 
-bfs_diff . -size 1 -size 511c -exec printf '511: %s\n' {} \; \
-    -o -size -2 -size 512c -exec printf '512: %s\n' {} \; \
-    -o -size +1 -size 513c -exec printf '513: %s\n' {} \;
+bfs_diff . -type f -size 1 -size 511c -exec printf '511: %s\n' {} \; \
+    -o -type f -size -2 -size 512c -exec printf '512: %s\n' {} \; \
+    -o -type f -size +1 -size 513c -exec printf '513: %s\n' {} \;
