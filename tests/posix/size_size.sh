tavianator/bfs

commit 5d1c2af7d1ca2cf77e0be712ea1e20a3e7188a94

opt: Apply data flow optimizations to more numeric ranges

opt.c
@@ -27,45 +27,157 @@ static char *fake_and_arg = "-a";
 static char *fake_or_arg = "-o";
 
 /**
+ * A contrained integer range.
+ */
+struct range {
+	/** The (inclusive) minimum value. */
+	long long min;
+	/** The (inclusive) maximum value. */
+	long long max;
+};
+
+/** Compute the minimum of two values. */
+static long long min_value(long long a, long long b) {
+	if (a < b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+/** Compute the maximum of two values. */
+static long long max_value(long long a, long long b) {
+	if (a > b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+/** Constrain the minimum of a range. */
+static void constrain_min(struct range *range, long long value) {
+	range->min = max_value(range->min, value);
+}
+
+/** Contrain the maximum of a range. */
+static void constrain_max(struct range *range, long long value) {
+	range->max = min_value(range->max, value);
+}
+
+/** Remove a single value from a range. */
+static void range_remove(struct range *range, long long value) {
+	if (range->min == value) {
+		if (range->min == LLONG_MAX) {
+			range->max = LLONG_MIN;
+		} else {
+			++range->min;
+		}
+	}
+
+	if (range->max == value) {
+		if (range->max == LLONG_MIN) {
+			range->min = LLONG_MAX;
+		} else {
+			--range->max;
+		}
+	}
+}
+
+/** Compute the union of two ranges. */
+static void range_union(struct range *result, const struct range *lhs, const struct range *rhs) {
+	result->min = min_value(lhs->min, rhs->min);
+	result->max = max_value(lhs->max, rhs->max);
+}
+
+/** Check if a range contains no values. */
+static bool range_impossible(const struct range *range) {
+	return range->min > range->max;
+}
+
+/** Set a range to contain no values. */
+static void set_range_impossible(struct range *range) {
+	range->min = LLONG_MAX;
+	range->max = LLONG_MIN;
+}
+
+/**
+ * Types of ranges we track.
+ */
+enum range_type {
+	/** Search tree depth. */
+	DEPTH_RANGE,
+	/** Group ID. */
+	GID_RANGE,
+	/** Inode number.  */
+	INUM_RANGE,
+	/** Hard link count. */
+	LINKS_RANGE,
+	/** File size. */
+	SIZE_RANGE,
+	/** User ID. */
+	UID_RANGE,
+	/** The number of range_types. */
+	MAX_RANGE,
+};
+
+/**
  * Data flow facts about an evaluation point.
  */
 struct opt_facts {
-	/** Minimum possible depth at this point. */
-	int mindepth;
-	/** Maximum possible depth at this point. */
-	int maxdepth;
+	/** The value ranges we track. */
+	struct range ranges[MAX_RANGE];
 
-	/** Bitmask of possible file types at this point. */
+	/** Bitmask of possible file types. */
 	enum bftw_typeflag types;
+	/** Bitmask of possible link target types. */
+	enum bftw_typeflag xtypes;
 };
+
+/** Initialize some data flow facts. */
+static void facts_init(struct opt_facts *facts) {
+	for (int i = 0; i < MAX_RANGE; ++i) {
+		struct range *range = facts->ranges + i;
+		range->min = 0; // All ranges we currently track are non-negative
+		range->max = LLONG_MAX;
+	}
+
+	facts->types = ~0;
+	facts->xtypes = ~0;
+}
 
 /** Compute the union of two fact sets. */
 static void facts_union(struct opt_facts *result, const struct opt_facts *lhs, const struct opt_facts *rhs) {
-	if (lhs->mindepth < rhs->mindepth) {
-		result->mindepth = lhs->mindepth;
-	} else {
-		result->mindepth = rhs->mindepth;
-	}
-
-	if (lhs->maxdepth > rhs->maxdepth) {
-		result->maxdepth = lhs->maxdepth;
-	} else {
-		result->maxdepth = rhs->maxdepth;
+	for (int i = 0; i < MAX_RANGE; ++i) {
+		range_union(result->ranges + i, lhs->ranges + i, rhs->ranges + i);
 	}
 
 	result->types = lhs->types | rhs->types;
+	result->xtypes = lhs->xtypes | rhs->xtypes;
 }
 
 /** Determine whether a fact set is impossible. */
 static bool facts_impossible(const struct opt_facts *facts) {
-	return facts->mindepth > facts->maxdepth || !facts->types;
+	for (int i = 0; i < MAX_RANGE; ++i) {
+		if (range_impossible(facts->ranges + i)) {
+			return true;
+		}
+	}
+
+	if (!facts->types || !facts->xtypes) {
+		return true;
+	}
+
+	return false;
 }
 
 /** Set some facts to be impossible. */
 static void set_facts_impossible(struct opt_facts *facts) {
-	facts->mindepth = INT_MAX;
-	facts->maxdepth = -1;
+	for (int i = 0; i < MAX_RANGE; ++i) {
+		set_range_impossible(facts->ranges + i);
+	}
+
 	facts->types = 0;
+	facts->xtypes = 0;
 }
 
 /**
@@ -110,6 +222,10 @@ static void debug_opt(const struct opt_state *state, const char *format, ...) {
 			case 'g':
 				cfprintf(cerr, "${ylw}%g${rs}", va_arg(args, double));
 				break;
+
+			default:
+				assert(false);
+				break;
 			}
 		} else {
 			fputc(*i, stderr);
@@ -117,55 +233,6 @@ static void debug_opt(const struct opt_state *state, const char *format, ...) {
 	}
 
 	va_end(args);
-}
-
-/** Update the inferred mindepth. */
-static void update_mindepth(struct opt_facts *facts, long long mindepth) {
-	if (mindepth > facts->mindepth) {
-		if (mindepth > INT_MAX) {
-			facts->maxdepth = -1;
-		} else {
-			facts->mindepth = mindepth;
-		}
-	}
-}
-
-/** Update the inferred maxdepth. */
-static void update_maxdepth(struct opt_facts *facts, long long maxdepth) {
-	if (maxdepth < facts->maxdepth) {
-		facts->maxdepth = maxdepth;
-	}
-}
-
-/** Infer data flow facts about a -depth N expression. */
-static void infer_depth_facts(struct opt_state *state, const struct expr *expr) {
-	switch (expr->cmp_flag) {
-	case CMP_EXACT:
-		update_mindepth(&state->facts_when_true, expr->idata);
-		update_maxdepth(&state->facts_when_true, expr->idata);
-		break;
-
-	case CMP_LESS:
-		update_maxdepth(&state->facts_when_true, expr->idata - 1);
-		update_mindepth(&state->facts_when_false, expr->idata);
-		break;
-
-	case CMP_GREATER:
-		if (expr->idata == LLONG_MAX) {
-			// Avoid overflow
-			state->facts_when_true.maxdepth = -1;
-		} else {
-			update_mindepth(&state->facts_when_true, expr->idata + 1);
-		}
-		update_maxdepth(&state->facts_when_false, expr->idata);
-		break;
-	}
-}
-
-/** Infer data flow facts about a -type expression. */
-static void infer_type_facts(struct opt_state *state, const struct expr *expr) {
-	state->facts_when_true.types &= expr->idata;
-	state->facts_when_false.types &= ~expr->idata;
 }
 
 /** Extract a child expression, freeing the outer expression. */
@@ -525,14 +592,65 @@ fail:
 	return NULL;
 }
 
+/** Infer data flow facts about an icmp-style ([+-]N) expression */
+static void infer_icmp_facts(struct opt_state *state, const struct expr *expr, enum range_type type) {
+	struct range *range_when_true = state->facts_when_true.ranges + type;
+	struct range *range_when_false = state->facts_when_false.ranges + type;
+	long long value = expr->idata;
+
+	switch (expr->cmp_flag) {
+	case CMP_EXACT:
+		constrain_min(range_when_true, value);
+		constrain_max(range_when_true, value);
+		range_remove(range_when_false, value);
+		break;
+
+	case CMP_LESS:
+		constrain_min(range_when_false, value);
+		constrain_max(range_when_true, value);
+		range_remove(range_when_true, value);
+		break;
+
+	case CMP_GREATER:
+		constrain_max(range_when_false, value);
+		constrain_min(range_when_true, value);
+		range_remove(range_when_true, value);
+		break;
+	}
+}
+
+/** Infer data flow facts about a -type expression. */
+static void infer_type_facts(struct opt_state *state, const struct expr *expr) {
+	state->facts_when_true.types &= expr->idata;
+	state->facts_when_false.types &= ~expr->idata;
+}
+
+/** Infer data flow facts about an -xtype expression. */
+static void infer_xtype_facts(struct opt_state *state, const struct expr *expr) {
+	state->facts_when_true.xtypes &= expr->idata;
+	state->facts_when_false.xtypes &= ~expr->idata;
+}
+
 static struct expr *optimize_expr_recursive(struct opt_state *state, struct expr *expr) {
 	state->facts_when_true = state->facts;
 	state->facts_when_false = state->facts;
 
 	if (expr->eval == eval_depth) {
-		infer_depth_facts(state, expr);
+		infer_icmp_facts(state, expr, DEPTH_RANGE);
+	} else if (expr->eval == eval_gid) {
+		infer_icmp_facts(state, expr, GID_RANGE);
+	} else if (expr->eval == eval_inum) {
+		infer_icmp_facts(state, expr, INUM_RANGE);
+	} else if (expr->eval == eval_links) {
+		infer_icmp_facts(state, expr, LINKS_RANGE);
+	} else if (expr->eval == eval_size) {
+		infer_icmp_facts(state, expr, SIZE_RANGE);
 	} else if (expr->eval == eval_type) {
 		infer_type_facts(state, expr);
+	} else if (expr->eval == eval_uid) {
+		infer_icmp_facts(state, expr, UID_RANGE);
+	} else if (expr->eval == eval_xtype) {
+		infer_xtype_facts(state, expr);
 	} else if (expr->eval == eval_not) {
 		expr = optimize_not_expr_recursive(state, expr);
 	} else if (expr->eval == eval_and) {
@@ -649,13 +767,13 @@ int optimize_cmdline(struct cmdline *cmdline) {
 
 	struct opt_state state = {
 		.cmdline = cmdline,
-		.facts = {
-			.mindepth = cmdline->mindepth,
-			.maxdepth = cmdline->maxdepth,
-			.types = ~0,
-		},
 		.facts_when_impure = &facts_when_impure,
 	};
+	facts_init(&state.facts);
+
+	struct range *depth = state.facts.ranges + DEPTH_RANGE;
+	depth->min = cmdline->mindepth;
+	depth->max = cmdline->maxdepth;
 
 	int optlevel = cmdline->optlevel;
 
@@ -675,13 +793,24 @@ int optimize_cmdline(struct cmdline *cmdline) {
 
 	cmdline->expr = ignore_result(&state, cmdline->expr);
 
-	if (optlevel >= 2 && facts_when_impure.mindepth > cmdline->mindepth) {
-		debug_opt(&state, "-O2: data flow: mindepth --> %d\n", facts_when_impure.mindepth);
-		cmdline->mindepth = facts_when_impure.mindepth;
+	const struct range *depth_when_impure = facts_when_impure.ranges + DEPTH_RANGE;
+	long long mindepth = depth_when_impure->min;
+	long long maxdepth = depth_when_impure->max;
+
+	if (optlevel >= 2 && mindepth > cmdline->mindepth) {
+		if (mindepth > INT_MAX) {
+			mindepth = INT_MAX;
+		}
+		cmdline->mindepth = mindepth;
+		debug_opt(&state, "-O2: data flow: mindepth --> %d\n", cmdline->mindepth);
 	}
-	if (optlevel >= 4 && facts_when_impure.maxdepth < cmdline->maxdepth) {
-		debug_opt(&state, "-O4: data flow: maxdepth --> %d\n", facts_when_impure.maxdepth);
-		cmdline->maxdepth = facts_when_impure.maxdepth;
+
+	if (optlevel >= 4 && maxdepth < cmdline->maxdepth) {
+		if (maxdepth < INT_MIN) {
+			maxdepth = INT_MIN;
+		}
+		cmdline->maxdepth = maxdepth;
+		debug_opt(&state, "-O4: data flow: maxdepth --> %d\n", cmdline->maxdepth);
 	}
 
 	return 0;

commit a6aecebeef8bb920c05043f392ac5ed80f676fa4

opt: Take the -size unit into account
Previously, expressions like

   $ bfs -size 1k -size 1024

were wrongly optimized to `-false` because 1 != 1024.

Fixes: 5d1c2af7 ("opt: Apply data flow optimizations to more numeric ranges")

src/eval.c
@@ -955,19 +955,8 @@ bool eval_size(const struct bfs_expr *expr, struct bfs_eval *state) {
 		return false;
 	}
 
-	static const off_t scales[] = {
-		[BFS_BLOCKS] = 512,
-		[BFS_BYTES] = 1,
-		[BFS_WORDS] = 2,
-		[BFS_KB] = 1LL << 10,
-		[BFS_MB] = 1LL << 20,
-		[BFS_GB] = 1LL << 30,
-		[BFS_TB] = 1LL << 40,
-		[BFS_PB] = 1LL << 50,
-	};
-
-	off_t scale = scales[expr->size_unit];
-	off_t size = (statbuf->size + scale - 1) / scale; // Round up
+	off_t factor = bfs_size_unit_factor(expr->size_unit);
+	off_t size = (statbuf->size + factor - 1) / factor; // Round up
 	return bfs_expr_cmp(expr, size);
 }
 

src/expr.c
@@ -14,6 +14,21 @@
 
 #include <string.h>
 
+off_t bfs_size_unit_factor(enum bfs_size_unit unit) {
+	static const off_t factors[] = {
+		[BFS_BLOCKS] = 512,
+		[BFS_BYTES] = 1,
+		[BFS_WORDS] = 2,
+		[BFS_KB] = 1LL << 10,
+		[BFS_MB] = 1LL << 20,
+		[BFS_GB] = 1LL << 30,
+		[BFS_TB] = 1LL << 40,
+		[BFS_PB] = 1LL << 50,
+	};
+
+	return factors[unit];
+}
+
 struct bfs_expr *bfs_expr_new(struct bfs_ctx *ctx, bfs_eval_fn *eval_fn, size_t argc, char **argv, enum bfs_kind kind) {
 	bfs_assert(kind != BFS_PATH);
 

src/expr.h
@@ -97,6 +97,9 @@ enum bfs_size_unit {
 	BFS_PB,
 };
 
+/** Get the scaling factor for a size unit. */
+off_t bfs_size_unit_factor(enum bfs_size_unit unit);
+
 /**
  * A linked list of expressions.
  */

src/opt.c
@@ -182,29 +182,54 @@ static void constrain_max(struct df_range *range, long long value) {
 	range->max = min_value(range->max, value);
 }
 
+/** Intersect two ranges. */
+static void range_intersect(struct df_range *range, const struct df_range *other) {
+	constrain_min(range, other->min);
+	constrain_max(range, other->max);
+}
+
 /** Constrain a range to a single value. */
 static void constrain_range(struct df_range *range, long long value) {
-	constrain_min(range, value);
-	constrain_max(range, value);
+	const struct df_range other = {
+		.min = value,
+		.max = value,
+	};
+	range_intersect(range, &other);
+}
+
+/** Check if a range contains a value. */
+static bool range_contains(const struct df_range *range, long long value) {
+	return value >= range->min && value <= range->max;
+}
+
+/** Subtract a range from another range. */
+static void range_subtract(struct df_range *range, const struct df_range *other) {
+	if (range_contains(other, range->min)) {
+		range->min = other->max;
+		if (range->min < LLONG_MAX) {
+			++range->min;
+		} else {
+			range->max = LLONG_MIN;
+		}
+	}
+
+	if (range_contains(other, range->max)) {
+		range->max = other->min;
+		if (range->max > LLONG_MIN) {
+			--range->max;
+		} else {
+			range->min = LLONG_MAX;
+		}
+	}
 }
 
 /** Remove a single value from a range. */
 static void range_remove(struct df_range *range, long long value) {
-	if (range->min == value) {
-		if (range->min == LLONG_MAX) {
-			range->max = LLONG_MIN;
-		} else {
-			++range->min;
-		}
-	}
-
-	if (range->max == value) {
-		if (range->max == LLONG_MIN) {
-			range->min = LLONG_MAX;
-		} else {
-			--range->max;
-		}
-	}
+	const struct df_range other = {
+		.min = value,
+		.max = value,
+	};
+	range_subtract(range, &other);
 }
 
 /** Compute the union of two ranges. */
@@ -1597,28 +1622,32 @@ static void data_flow_pred(struct bfs_opt *opt, enum pred_type pred, bool value)
 
 /** Transfer function for icmp-style ([+-]N) expressions. */
 static void data_flow_icmp(struct bfs_opt *opt, const struct bfs_expr *expr, enum range_type type) {
-	struct df_range *true_range = &opt->after_true.ranges[type];
-	struct df_range *false_range = &opt->after_false.ranges[type];
 	long long value = expr->num;
+	struct df_range range;
 
 	switch (expr->int_cmp) {
 	case BFS_INT_EQUAL:
-		constrain_range(true_range, value);
-		range_remove(false_range, value);
+		range.min = range.max = value;
 		break;
 
 	case BFS_INT_LESS:
-		constrain_min(false_range, value);
-		constrain_max(true_range, value);
-		range_remove(true_range, value);
+		range.min = LLONG_MIN;
+		range.max = value;
+		range_remove(&range, value);
 		break;
 
 	case BFS_INT_GREATER:
-		constrain_max(false_range, value);
-		constrain_min(true_range, value);
-		range_remove(true_range, value);
+		range.min = value;
+		range.max = LLONG_MAX;
+		range_remove(&range, value);
 		break;
 	}
+
+	struct df_range *true_range = &opt->after_true.ranges[type];
+	range_intersect(true_range, &range);
+
+	struct df_range *false_range = &opt->after_false.ranges[type];
+	range_subtract(false_range, &range);
 }
 
 /** Transfer function for -{execut,read,writ}able. */
@@ -1710,8 +1739,54 @@ static struct bfs_expr *data_flow_samefile(struct bfs_opt *opt, struct bfs_expr 
 
 /** Transfer function for -size. */
 static struct bfs_expr *data_flow_size(struct bfs_opt *opt, struct bfs_expr *expr, const struct visitor *visitor) {
-	struct df_range *range = &opt->after_true.ranges[SIZE_RANGE];
-	if (range->min == range->max) {
+	long long size = expr->num;
+	long long factor = bfs_size_unit_factor(expr->size_unit);
+
+	struct df_range range;
+	range_init_bottom(&range);
+
+	if (size - 1 <= LLONG_MAX / factor) {
+		range.min = factor * (size - 1);
+
+		if (range.min <= LLONG_MAX - factor) {
+			range.max = range.min + factor;
+		} else {
+			range.max = LLONG_MAX;
+		}
+
+		range_remove(&range, range.min);
+	}
+
+	switch (expr->int_cmp) {
+	case BFS_INT_EQUAL:
+		break;
+
+	case BFS_INT_LESS:
+		if (range_is_bottom(&range)) {
+			range_init_top(&range);
+		} else {
+			range.max = range.min;
+			range.min = LLONG_MIN;
+			range_remove(&range, range.max);
+		}
+		break;
+
+	case BFS_INT_GREATER:
+		if (!range_is_bottom(&range)) {
+			range.min = range.max;
+			range.max = LLONG_MAX;
+			range_remove(&range, range.min);
+		}
+		break;
+	}
+
+	struct df_range *true_range = &opt->after_true.ranges[SIZE_RANGE];
+	range_intersect(true_range, &range);
+
+	struct df_range *false_range = &opt->after_false.ranges[SIZE_RANGE];
+	range_subtract(false_range, &range);
+
+	if (true_range->min == true_range->max) {
 		expr->probability = 0.01;
 	} else {
 		expr->probability = 0.5;
@@ -1892,7 +1967,6 @@ static struct bfs_expr *data_flow_visit(struct bfs_opt *opt, struct bfs_expr *ex
 		{eval_gid,     GID_RANGE},
 		{eval_inum,   INUM_RANGE},
 		{eval_links, LINKS_RANGE},
-		{eval_size,   SIZE_RANGE},
 		{eval_uid,     UID_RANGE},
 	};
 

tests/common/size_big.sh
@@ -1,1 +1,1 @@
-bfs_diff basic -size 9223372036854775807
+bfs_diff basic -type f -size 9223372036854775807

tests/common/size_minus_big.out
@@ -1,0 +1,7 @@
+basic/a
+basic/b
+basic/c/d
+basic/e/f
+basic/j/foo
+basic/k/foo/bar
+basic/l/foo/bar/baz

tests/common/size_minus_big.sh
@@ -1,0 +1,1 @@
+bfs_diff basic -type f -size -9223372036854775807

tests/common/size_plus_big.sh
@@ -1,0 +1,1 @@
+bfs_diff basic -type f -size +9223372036854775807

tests/posix/size_minus_1.out
@@ -1,0 +1,6 @@
+basic/a
+basic/b
+basic/c/d
+basic/e/f
+basic/j/foo
+basic/k/foo/bar

tests/posix/size_minus_1.sh
@@ -1,0 +1,1 @@
+bfs_diff basic -type f -size -1

tests/posix/size_size.out
@@ -1,0 +1,3 @@
+511: ./511
+512: ./512
+513: ./513

tests/posix/size_size.sh
@@ -1,0 +1,11 @@
+# Regression test: the optimizer used to ignore -size units
+
+cd "$TEST"
+
+dd if=/dev/null of=511 bs=1 seek=511 count=0
+dd if=/dev/null of=512 bs=1 seek=512 count=0
+dd if=/dev/null of=513 bs=1 seek=513 count=0
+
+bfs_diff . -size 1 -size 511c -exec printf '511: %s\n' {} \; \
+    -o -size -2 -size 512c -exec printf '512: %s\n' {} \; \
+    -o -size +1 -size 513c -exec printf '513: %s\n' {} \;