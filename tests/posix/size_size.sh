973cab29ef8f71449dec13a2b4fde6facd9b12d3
Tavian Barnes <tavianator@tavianator.com>
2026-06-29 13:38:58 -0400

    tests/posix/size_size: Don't do dd if=/dev/null
    NetBSD and DragonFly BSD don't seem to like it.
    
    Link: https://github.com/tavianator/bfs/actions/runs/28390678813/job/84116902580

--- tests/posix/size_size.sh
+++ tests/posix/size_size.sh
@@ -2,9 +2,11 @@
 
 cd "$TEST"
 
-dd if=/dev/null of=511 bs=1 seek=511 count=0
-dd if=/dev/null of=512 bs=1 seek=512 count=0
-dd if=/dev/null of=513 bs=1 seek=513 count=0
+printf '.' >./1
+
+dd if=./1 of=./511 bs=1 seek=510
+dd if=./1 of=./512 bs=1 seek=511
+dd if=./1 of=./513 bs=1 seek=512
 
 bfs_diff . -type f -size 1 -size 511c -exec printf '511: %s\n' {} \; \
     -o -type f -size -2 -size 512c -exec printf '512: %s\n' {} \; \
